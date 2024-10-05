f0c51fb98954c575e5a29a3bc4bf5b686d1762bd
Gaik Artur <141954990+webDevArtur@users.noreply.github.com>
2024-10-05 19:51:25 +0300

    Update vite.config.js

--- simple-chat/vite.config.js
+++ simple-chat/vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/2024-2-VK-EDU-Frontend-A-Gaik/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        chat: resolve(__dirname, 'chat.html'),
+      },
+    },
+  },
 });
