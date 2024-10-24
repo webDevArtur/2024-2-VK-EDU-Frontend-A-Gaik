be4b2a229fa69523d1ba3b8b6aab1ff1dd4c7130
Гайк Артур Сергеевич <gaikas@sibur.ru>
2024-10-24 03:43:25 +0300

    5_homework

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/2024-2-VK-EDU-Frontend-A-Gaik/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 });
