4abb400de80c9dfd5fb17803cedf0d0eea1c4039
Gaik Artur <141954990+webDevArtur@users.noreply.github.com>
2024-10-28 23:16:01 +0300

    Delete correctSentence.test.js

--- correctSentence.test.js
+++
@@ -1,9 +0,0 @@
-import correctSentence from "./correctSentence";
-
-test("returns correct sentence", () => {
-  expect(correctSentence("greetings, friends")).toBe("Greetings, friends.");
-  expect(correctSentence("Greetings, friends")).toBe("Greetings, friends.");
-  expect(correctSentence("Greetings, friends.")).toBe("Greetings, friends.");
-  expect(correctSentence("")).toBe("");
-  expect(correctSentence("   ")).toBe("");
-});
