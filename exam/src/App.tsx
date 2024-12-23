9216ca9177f3428d95c8c349d5eaabd3cea0ae15
Гайк Артур Сергеевич <gaikas@sibur.ru>
2024-12-23 22:24:04 +0300

    exam

--- exam/src/App.tsx
+++ exam/src/App.tsx
@@ -1,14 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage/MainPage';
 import HistoryPage from './pages/HistoryPage/HistoryPage';
 
 const App: React.FC = () => (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/history" element={<HistoryPage />} />
-        </Routes>
-    </BrowserRouter>
+  <Routes>
+    <Route path="/" element={<MainPage />} />
+    <Route path="/history" element={<HistoryPage />} />
+  </Routes>
 );
 
 export default App;
