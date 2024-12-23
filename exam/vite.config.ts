c305cc827cf3773d46b5b2a8c5b55d870e56e4c8
Гайк Артур Сергеевич <gaikas@sibur.ru>
2024-12-23 21:57:10 +0300

    exam

--- exam/vite.config.ts
+++ exam/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/2024-2-VK-EDU-Frontend-A-Gaik/",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://vkedu-fullstack-div2.ru/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
